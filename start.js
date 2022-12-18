8d55dd41600fac1370755f355374f38bcdd15b7e
LaupWing <loc_strict@hotmail.com>
2022-12-18 16:56:34 +0100

    udaptpe

--- start.js
+++ start.js
@@ -17,7 +17,7 @@ const options = {
          // first: "Cover page",
          // 2: "Second page", // Any page number is working. 1-based index
          default:
-            '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            '<span style="color: red; ml-auto; width: 100%; display:flex;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
          last: "Last Page",
       },
    },
