e0ca4cf9ad2977296a0cb102728e79106c0a20ba
LaupWing <loc_strict@hotmail.com>
2022-12-18 16:59:37 +0100

    update

--- start.js
+++ start.js
@@ -8,16 +8,16 @@ const options = {
    orientation: "portrait",
    border: "10mm",
    header: {
-      height: "20mm",
+      height: "28mm",
       contents: '<div style="text-align: center;">Author: Shyam Hajare</div>',
    },
    footer: {
-      height: "20mm",
+      height: "28mm",
       contents: {
          // first: "Cover page",
          // 2: "Second page", // Any page number is working. 1-based index
          default:
-            '<span style="color: red; ml-auto; width: 100%; display:flex;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            '<span style="color: red; ml-auto; width: 100%; display:flex; justify-content: end;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
          last: "Last Page",
       },
    },
